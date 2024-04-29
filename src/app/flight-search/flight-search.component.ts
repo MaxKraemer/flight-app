18b18f3885d40c3da1f508eaf5e4ab068159691b
Max Kraemer <maximilian.kraemer@gmx.de>
2024-04-29 13:33:52 +0200

    create object

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Flight } from '../flight';
 
 @Component({
   selector: 'app-flight-search',
@@ -7,6 +8,20 @@ import { Component } from '@angular/core';
   templateUrl: './flight-search.component.html',
   styleUrl: './flight-search.component.scss'
 })
-export class FlightSearchComponent {
+export class FlightSearchComponent implements OnInit {
 
+  flights : Flight = {
+    id: 1,
+    from: 'Mumbai',
+    to: 'Delhi',
+    date: '2021-12-12',
+    delayed: false
+  };
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+   
+  }
 }
