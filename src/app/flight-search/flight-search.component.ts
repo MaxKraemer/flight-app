2ede4d3820ba6e0903427fdf8f2f4f689c1655f0
Max Kraemer <maximilian.kraemer@gmx.de>
2024-04-30 10:08:06 +0200

    search flights

--- src/app/flight-search/flight-search.component.ts
+++ src/app/flight-search/flight-search.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Flight } from '../flight';
 import { ApiService } from '../services/api.service';
+import { CommonModule, DatePipe } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-flight-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DatePipe, FormsModule, ReactiveFormsModule],
   templateUrl: './flight-search.component.html',
   styleUrl: './flight-search.component.scss'
 })
 export class FlightSearchComponent implements OnInit {
-
-  flights : Flight = {
+  flights: Flight = {
     id: 1,
     from: 'Mumbai',
     to: 'Delhi',
@@ -19,15 +20,33 @@ export class FlightSearchComponent implements OnInit {
     delayed: false
   };
 
-  fligthData: any;
+  flightForm = new FormGroup({
+    from: new FormControl('', Validators.required),
+    to: new FormControl('', Validators.required),
+  });
+
+  flightData: Flight[] = [];
+  filteredFlights: Flight[] = [];
+  showflight: boolean = false;
 
   constructor(private apiService: ApiService) { }
 
-
   ngOnInit(): void {
     this.apiService.callApiHandler().subscribe(data => {
-      this.fligthData = data[0];
-      console.log(this.fligthData, 'flights data');
+      this.flightData = data;
+      this.filteredFlights = data;
+      console.log(this.flightData, 'flights data');
+    });
+
+    this.flightForm.valueChanges.subscribe(() => {
+      this.searchFlights();
+    });
+  }
+
+  searchFlights() {
+    const formValue = this.flightForm.value;
+    this.filteredFlights = this.flightData.filter((flight: Flight) => {
+      return flight.from === formValue.from && flight.to === formValue.to;
     });
   }
 }
